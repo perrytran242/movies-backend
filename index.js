c3e745efd9352899597d644487a512f0f01d3eb8
perrytran242 <ptran209@gmail.com>
2020-06-10 11:32:31 -0700

    created endpoint for search query

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ require('dotenv').config();
 const express = require("express");
 const axios = require('axios');
 const path = require("path");
+const bodyParser = require("body-parser")
+
 
 /**
  * App Variables
@@ -16,25 +18,39 @@ const port = process.env.PORT || "8000";
  *  App Configuration
  */
 
-
+app.use(bodyParser.json());
 
 /**
  * Routes Definitions
  */
 
 app.get("/", (req, res) => {
-    
-
 });
 
 
-app.get('/getMovies', (req, res) => {
+app.post('/getMovies', (req, res) => {  
+    console.log(req.body)      
+    const { searchTerm } = req.body;
+    console.log('----SEARCH TERM----');
+    console.log(searchTerm);
+    // const parsedBody = JSON.parse(req.body.searchTerm);
+    // console.log(parsedBody);
+    axios.get(`${process.env.MOVIE_URL}${process.env.API_KEY}&language=en-US&page=1&query=${searchTerm}/`)
+        .then((response) => {
+            console.log('----RESPONSE----');
+            console.log(response.data);        
+            const { data } = response;
+            // let stringify = JSON.stringify(response)
+            // res.send(JSON.stringify({data: response}));
+            res.json({data})
+    })
+    .catch(err => {
+        console.log("-----ERROR-----");
+        console.log(err);        
 
-    axios.get(`${process.env.MOVIE_URL}?api_key=${process.env.API_KEY}`).then(res => {
-        console.log('res:'. res);
-    })        
-
-
+        res.send(err);
+    });
+    
 });
 /**
  * Server Activation
